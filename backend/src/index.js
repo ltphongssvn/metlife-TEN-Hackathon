e1e938a4cd6822846d3ef3cb224900a5b65f3785
Thanh Phong Le <ltphongssvn@gmail.com>
2025-11-03 07:24:29 -0800

    feat: Add backend/src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlenmtcoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // Root route
 app.get('/', (req, res) => {
